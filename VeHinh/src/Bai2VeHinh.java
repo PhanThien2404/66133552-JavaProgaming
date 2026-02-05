cb3ab613e572603458dbb627d64457e18c3c1325
ThienPhan2404 <thien.pnn.66cntt@ntu.edu.vn>
2026-02-05 08:04:25 +0700

    VeHinh

---
+++ VeHinh/src/Bai2VeHinh.java
@@ -0,0 +1,10 @@
+
+public class Bai2VeHinh {
+
+	public static void main(String[] args) {
+		        System.out.println("    * ");
+		        System.out.println("   *** ");
+		        System.out.println("  ***** ");
+		        System.out.println("*********");
+		    }
+	}
