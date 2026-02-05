94a1df498ec5de8424d1479c1aca18de260c5335
ThienPhan2404 <thien.pnn.66cntt@ntu.edu.vn>
2026-02-05 07:30:47 +0700

    Hello

---
+++ Bai1_Hello/src/SayHelloApp.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class SayHelloApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	  System.out.println("Xin chao, toi la cac sv khoa 66 CNTT - NTU: Hoc Lap trinh JAVA");
+	  System.out.println("Mai Cường Thọ");
+	}
+
+}
