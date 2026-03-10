a550f218967f8884a4aeb0cc8b215dbb194f8486
ThienPhan2404 <thien.pnn.66cntt@ntu.edu.vn>
2026-03-10 21:27:32 +0700

    Ex_Account_1_6

---
+++ Bai6_Exercises on Classes/src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Ex_Account_1_6 a1 = new Ex_Account_1_6("A101", "Tan Ah Teck", 88);
+        System.out.println(a1); 
+
+        Ex_Account_1_6 a2 = new Ex_Account_1_6("A102", "Kumar"); 
+        System.out.println(a2);
+
+        System.out.println("ID: " + a1.getID());
+        System.out.println("Name: " + a1.getName());
+        System.out.println("Balance: " + a1.getBalance());
+
+        a1.credit(100);
+        System.out.println(a1);
+        a1.debit(50);
+        System.out.println(a1);
+        a1.debit(500); 
+        System.out.println(a1);
+
+        a1.transferTo(a2, 100); 
+        System.out.println(a1);
+        System.out.println(a2);
+	}
+
+}
