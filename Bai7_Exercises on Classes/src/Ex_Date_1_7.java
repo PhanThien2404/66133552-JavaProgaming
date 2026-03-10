c4e354f5e1648506129ae84ca3c54897fac61acf
ThienPhan2404 <thien.pnn.66cntt@ntu.edu.vn>
2026-03-10 21:28:15 +0700

    Ex_Date_1_7

---
+++ Bai7_Exercises on Classes/src/Ex_Date_1_7.java
@@ -0,0 +1,30 @@
+public class Ex_Date_1_7 {
+    private int day;
+    private int month;
+    private int year;
+
+    public Ex_Date_1_7(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() { return day; }
+    public int getMonth() { return month; }
+    public int getYear() { return year; }
+    
+    public void setDay(int day) { this.day = day; }
+    public void setMonth(int month) { this.month = month; }
+    public void setYear(int year) { this.year = year; }
+
+    public void setDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d/%02d/%04d", day, month, year);
+    }
+}
