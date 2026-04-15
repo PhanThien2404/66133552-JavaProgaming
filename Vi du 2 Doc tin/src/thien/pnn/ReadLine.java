191c9470c6d89b20a9a0f8efce728057b12dc91c
ThienPhan2404 <thien.pnn.66cntt@ntu.edu.vn>
2026-04-15 21:16:07 +0700

    Vi du 2

---
+++ Vi du 2 Doc tin/src/thien/pnn/ReadLine.java
@@ -0,0 +1,20 @@
+package thien.pnn;
+
+import java.io.*;
+
+class ReadLine
+{
+    public static void main(String args[]) throws IOException
+    {
+        // create a BufferedReader using System.in
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        System.out.println("Nhap chuoi.");
+        System.out.println("Nhap 'stop' ket thuc chuong trinh.");
+        do
+        {
+            str = br.readLine();
+            System.out.println(str);
+        } while(!str.equals("stop"));
+    }
+}
