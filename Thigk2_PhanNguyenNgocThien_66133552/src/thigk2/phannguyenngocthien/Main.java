1f22f7a4b008f7d98b37d850eee128f9d418cd22
ThienPhan2404 <thien.pnn.66cntt@ntu.edu.vn>
2026-04-23 07:35:02 +0700

    COMMIT LẦN THỨ 2

--- Thigk2_PhanNguyenNgocThien_66133552/src/thigk2/phannguyenngocthien/Main.java
+++ Thigk2_PhanNguyenNgocThien_66133552/src/thigk2/phannguyenngocthien/Main.java
@@ -1,9 +1,16 @@
 package thigk2.phannguyenngocthien;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- Thigk2_PhanNguyenNgocThien_66133552/src/thigk2/phannguyenngocthien/SinhVien.java
+++ Thigk2_PhanNguyenNgocThien_66133552/src/thigk2/phannguyenngocthien/SinhVien.java
@@ -1,5 +1,44 @@
 package thigk2.phannguyenngocthien;
 
 public class SinhVien {
-
+   private String maSV;
+   private String hoTen;
+   private int namSinh;
+   private String lop;
+   public SinhVien(String maSV, String hoTen, int namSinh, String lop) {
+	this.maSV = maSV;
+	this.hoTen = hoTen;
+	this.namSinh = namSinh;
+	this.lop = lop;
+   }
+   public String getMaSV() {
+	return maSV;
+   }
+   public void setMaSV(String maSV) {
+	this.maSV = maSV;
+   }
+   public String getHoTen() {
+	return hoTen;
+   }
+   public void setHoTen(String hoTen) {
+	this.hoTen = hoTen;
+   }
+   public int getNamSinh() {
+	return namSinh;
+   }
+   public void setNamSinh(int namSinh) {
+	this.namSinh = namSinh;
+   }
+   public String getLop() {
+	return lop;
+   }
+   public void setLop(String lop) {
+	this.lop = lop;
+   }
+   @Override
+   public String toString() {
+	return "SinhVien [maSV=" + maSV + ", hoTen=" + hoTen + ", namSinh=" + namSinh + ", lop=" + lop + "]";
+   }
+   
+   
 }
